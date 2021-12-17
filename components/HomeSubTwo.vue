f6c90647b2a62c8ffb611919c9b23763933b1cd2
hattacr7 <zulhatta@bravonet.my>
2021-12-17 16:07:06 +0800

    init-thefirstfile

---
+++ components/HomeSubTwo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <h4>
+            {{ cardsSection.title }}
+        </h4>
+        <p>
+            {{ cardsSection.snippet }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['cardsSection']
+    }
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
